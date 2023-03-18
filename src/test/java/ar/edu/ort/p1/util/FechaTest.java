1a11b2ded774633f4e2ed8ade7e6a72acb7e0372
Ignacio Sanchez <sanchezih@gmail.com>
2023-03-18 18:38:25 -0300

    Update FechaTest.java

--- src/test/java/ar/edu/ort/p1/util/FechaTest.java
+++ src/test/java/ar/edu/ort/p1/util/FechaTest.java
@@ -1,3 +1,5 @@
+package ar.edu.ort.p1.util;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,8 +9,6 @@ public class FechaTest {
 	@DisplayName("Validar ...")
 	@Test
 	public void test1() {
-//		UsuarioDto esperado = new UsuarioDto(3L, "Martin");
-//		UsuarioDto resultado = usuarioServicio.crearUsuario(3L, "Martin");
 
 		Assertions.assertEquals(1, 1);
 	}
