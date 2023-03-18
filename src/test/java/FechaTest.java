7d028e29b8b3b6ec599bceb9cfcc4a925ba1d2a6
Ignacio Sanchez <sanchezih@gmail.com>
2023-03-18 17:10:21 -0300

    add 1st test

---
+++ src/test/java/FechaTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FechaTest {
+
+	@DisplayName("Validar ...")
+	@Test
+	public void test1() {
+//		UsuarioDto esperado = new UsuarioDto(3L, "Martin");
+//		UsuarioDto resultado = usuarioServicio.crearUsuario(3L, "Martin");
+
+		Assertions.assertEquals(1, 1);
+	}
+}
