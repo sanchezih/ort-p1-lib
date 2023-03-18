edd2023fe4c50dc1d30fde7ba2fcbb76c3f75daa
Ignacio Sanchez <sanchezih@gmail.com>
2023-03-18 19:19:58 -0300

    Update FechaTest.java

--- src/test/java/ar/edu/ort/p1/util/FechaTest.java
+++ src/test/java/ar/edu/ort/p1/util/FechaTest.java
@@ -1,5 +1,8 @@
 package ar.edu.ort.p1.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,9 @@ public class FechaTest {
 	@Test
 	public void test1() {
 
-		Assertions.assertEquals(1, 1);
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		Date date = new Date();
+
+		Assertions.assertEquals(Fecha.hoy().toString(), formatter.format(date));
 	}
 }
